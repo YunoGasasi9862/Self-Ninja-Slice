b564f4857f151f8cbc5933250facc51115956d92
Muhammad Bilal <m.bilal9862@gmail.com>
2022-06-12 12:39:29 +0300

    ss

--- Assets/Fruit.cs
+++ Assets/Fruit.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+    Rigidbody2D rb;
+    public GameObject watermelonSliced;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.tag=="Blade")
+        {
+            Vector3 direction = (collision.transform.position - transform.position).normalized;
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            GameObject sliced = Instantiate(watermelonSliced, transform.position, rotation);
+            Destroy(sliced, 3f);
+            Destroy(gameObject);
+
+
+        }
     }
 }
