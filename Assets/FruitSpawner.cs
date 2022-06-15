fdbc7668f8a1e3c24430929f8be5a1d241dfa387
Muhammad Bilal <m.bilal9862@gmail.com>
2022-06-15 13:26:58 +0300

    ss

--- Assets/FruitSpawner.cs
+++ Assets/FruitSpawner.cs
@@ -25,7 +25,10 @@ public class FruitSpawner : MonoBehaviour
         {
             float delay = Random.Range(min, max);
             yield return new WaitForSeconds(2);
-
+            int spawnIndex = Random.Range(0, SpawnPoints.Length);
+            Transform spawnpoint = SpawnPoints[spawnIndex];
+            GameObject spawned= Instantiate(fruit, spawnpoint.position, spawnpoint.rotation);
+            Destroy(spawned, 5f);
 
 
 
